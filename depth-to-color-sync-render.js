75062d1dd47e4c041a8e66e4e0c895860d1fd51d
Aleksandar Stojiljkovic <aleksandar.stojiljkovic@intel.com>
2018-08-28 07:30:26 +0300

    415 color to depth support

--- depth-to-color-sync-render.js
+++ depth-to-color-sync-render.js
@@ -165,8 +165,8 @@ class DepthToColorSyncRender {
 
     const render = this.programs.render;
     gl.useProgram(render);
-    const lastReduceBlack = textures.reduceBlack[0/*textures.reduceBlack.length - 1*/];
-    gl.uniform1i(gl.getUniformLocation(render, "s"), lastReduceBlack.unit);
+    const lastReduceBlack = textures.reduceBlack[4/*textures.reduceBlack.length - 1*/];
+    gl.uniform1i(gl.getUniformLocation(render, "s"), textures.noHoles.unit/*lastReduceBlack.unit*/);
     gl.uniform2f(gl.getUniformLocation(render, 'range'), range[0], range[1]);    
   }
 
@@ -205,6 +205,24 @@ class DepthToColorSyncRender {
       #define BSIGMA 0.1
       #define MSIZE 15
 
+      const float Epsilon = 1e-10;
+
+      vec3 RGBtoHCV(vec3 RGB)
+      {
+        // Based on work by Sam Hocevar and Emil Persson
+        vec4 P = (RGB.g < RGB.b) ? vec4(RGB.bg, -1.0, 2.0/3.0) : vec4(RGB.gb, 0.0, -1.0/3.0);
+        vec4 Q = (RGB.r < P.x) ? vec4(P.xyw, RGB.r) : vec4(RGB.r, P.yzx);
+        float C = Q.x - min(Q.w, Q.y);
+        float H = abs((Q.w - Q.y) / (6.0 * C + Epsilon) + Q.z);
+        return vec3(H, C, Q.x);
+      }
+
+      vec3 RGBtoHSV(vec3 RGB) {
+        vec3 HCV = RGBtoHCV(RGB);
+        float S = HCV.y / (HCV.z + Epsilon);
+        return vec3(HCV.x, S, HCV.z);
+      }
+
       float normpdf3(vec3 v, float sigma)
       {
       	return 0.39894*exp(-0.5*dot(v,v)/(sigma*sigma))/sigma;
@@ -231,6 +249,7 @@ class DepthToColorSyncRender {
       	  }
       	}
       	fragColor = vec4(final_colour/Z, 1.0);
+        // fragColor.rgb = RGBtoHSV(fragColor.rgb);
       }`;
 
     
@@ -272,7 +291,7 @@ class DepthToColorSyncRender {
         vec2 v = position2d * depthFocalLength + depthOffset;
         float z = texture(sDepth, v).r * depthScale;
         z = (z == 1.0) ? 0.0 : z;
-        fragColor.a = (z > 0.97) ? 0.97 : z;
+        fragColor.a = (z > 0.95) ? 0.95 : z;
 
 /*        if (col.a > 0.0) {
 //          return;
@@ -302,7 +321,7 @@ class DepthToColorSyncRender {
         vec4 z1 = vec4(posr.a, posbr.a, posb.a, posbl.a);
         vec4 nonzero = sign(z1 * z0);
         float depth = dot(nonzero, mix(z1, z0, 0.5)) / dot(nonzero, nonzero);
-        fragColor = vec4(col.rgb, depth > 0.97 ? 0.97 : depth);*/
+        fragColor = vec4(col.rgb, depth > 0.95 ? 0.95 : depth);*/
       }`;
 
     const reduceBlackVertex = `#version 300 es
@@ -339,10 +358,12 @@ class DepthToColorSyncRender {
         if (c.a > range.y)
           return;
 
-        vec4 similarThreshold = vec4(c.a > 0.0 ? 0.02 : 0.03);
+        vec4 similarThreshold = vec4(c.a > 0.0 ? 0.05 : 0.05);
 
-        vec4 dd3 = vec4(dd,dd) * vec4(11.0, 11.0, -11.0, 0.0);
-        vec4 dd4 = vec4(dd,dd) * vec4(15.0, 15.0, -15.0, 0.0);
+        vec4 dd3 = vec4(dd,dd) * vec4(6.0, 6.0, -6.0, 0.0);
+        vec4 dd4 = vec4(dd,dd) * vec4(8.0, 8.0, -8.0, 0.0);
+        vec4 dd6 = 3.0 * dd3;
+        vec4 dd8 = 3.0 * dd4;
 
         vec4 c1 = normalizeRG(texture(s, t - dd3.rg));
         vec4 c5 = normalizeRG(texture(s, t + dd3.rg));
@@ -354,8 +375,17 @@ class DepthToColorSyncRender {
         vec4 c4 = normalizeRG(texture(s, t + dd4.ra));
 
         vec4 cn = normalizeRG(c);
+
+        vec4 c11 = normalizeRG(texture(s, t - dd6.rg));
+        vec4 c51 = normalizeRG(texture(s, t + dd6.rg));
+        vec4 c31 = normalizeRG(texture(s, t - dd6.bg));
+        vec4 c71 = normalizeRG(texture(s, t + dd6.bg));
+        vec4 c21 = normalizeRG(texture(s, t - dd8.ag));
+        vec4 c61 = normalizeRG(texture(s, t + dd8.ag));
+        vec4 c81 = normalizeRG(texture(s, t - dd8.ra));
+        vec4 c41 = normalizeRG(texture(s, t + dd8.ra));
         
-        // c1 and c5, c2 and c6,... are opposite.
+        // Closer pixels first; c1 and c5, c2 and c6,... are opposite.
         vec4 z0 = vec4(c1.a, c2.a, c3.a, c4.a);
         vec4 z1 = vec4(c5.a, c6.a, c7.a, c8.a);
         vec4 bckgndThreshold = vec4(range.y);
@@ -365,30 +395,55 @@ class DepthToColorSyncRender {
         vec4 foreground2 = vec4(1.0) - background2;
         bool backgroundBorder = any(greaterThan(background1 + background2, vec4(0.0)));
 
+        // Distant pixels.
+        vec4 z01 = vec4(c11.a, c21.a, c31.a, c41.a);
+        vec4 z11 = vec4(c51.a, c61.a, c71.a, c81.a);
+        vec4 background11 = vec4(greaterThan(z01, bckgndThreshold));
+        vec4 background21 = vec4(greaterThan(z11, bckgndThreshold));
+        vec4 foreground11 = vec4(1.0) - background11;
+        vec4 foreground21 = vec4(1.0) - background21;
+
+        // Current pixel values, packed for parallel diff.
+        vec4 rn = vec4(cn.r);
+        vec4 gn = vec4(cn.g);
+        vec4 bn = vec4(cn.b);
+
         vec4 r1 = vec4(c1.r, c2.r, c3.r, c4.r);
         vec4 r2 = vec4(c5.r, c6.r, c7.r, c8.r);
         vec4 g1 = vec4(c1.g, c2.g, c3.g, c4.g);
         vec4 g2 = vec4(c5.g, c6.g, c7.g, c8.g);
         vec4 b1 = vec4(c1.b, c2.b, c3.b, c4.b);
         vec4 b2 = vec4(c5.b, c6.b, c7.b, c8.b);
-        vec4 rn = vec4(cn.r);
-        vec4 gn = vec4(cn.g);
-        vec4 bn = vec4(cn.b);
         vec4 diffr1 = abs(r1 - rn);
         vec4 diffr2 = abs(r2 - rn);
         vec4 diffg1 = abs(g1 - gn);
         vec4 diffg2 = abs(g2 - gn);
         vec4 diffb1 = abs(b1 - bn);
         vec4 diffb2 = abs(b2 - bn);
+        // Use RGB for first pass to evaluate similar colors.
+        vec4 sim1 = vec4(lessThan(max(max(diffr1, diffg1), diffb1), similarThreshold));
+        vec4 sim2 = vec4(lessThan(max(max(diffr2, diffg2), diffb2), similarThreshold));
 
-        
-        if (backgroundBorder) {
+        vec4 r11 = vec4(c11.r, c21.r, c31.r, c41.r);
+        vec4 r21 = vec4(c51.r, c61.r, c71.r, c81.r);
+        vec4 g11 = vec4(c11.g, c21.g, c31.g, c41.g);
+        vec4 g21 = vec4(c51.g, c61.g, c71.g, c81.g);
+        vec4 b11 = vec4(c11.b, c21.b, c31.b, c41.b);
+        vec4 b21 = vec4(c51.b, c61.b, c71.b, c81.b);
+        vec4 diffr11 = abs(r11 - rn);
+        vec4 diffr21 = abs(r21 - rn);
+        vec4 diffg11 = abs(g11 - gn);
+        vec4 diffg21 = abs(g21 - gn);
+        vec4 diffb11 = abs(b11 - bn);
+        vec4 diffb21 = abs(b21 - bn);
+        vec4 sim11 = vec4(lessThan(max(max(diffr11, diffg11), diffb11), similarThreshold));
+        vec4 sim21 = vec4(lessThan(max(max(diffr21, diffg21), diffb21), similarThreshold));
+
+/*        if (backgroundBorder) {
           // We need to know which one is background border and for that color
           // to check if it is similar to background.
-          vec4 sim1 = vec4(lessThan(max(max(diffr1, diffg1), diffb1), similarThreshold));
-          vec4 sim2 = vec4(lessThan(max(max(diffr2, diffg2), diffb2), similarThreshold));
           float background = dot(sim1, background1) + dot(sim2, background2);
-          fragColor.a = (background > 0.0) ? 1.0 : fragColor.a;
+          // fragColor.a = (background > 0.0) ? 1.0 : fragColor.a;
           // TODO: in case that the color is in the middle of background pixels
           // even it is not of the same color, we threat it as background when
           // the depth is 0.
@@ -396,15 +451,80 @@ class DepthToColorSyncRender {
                                    dot(background2, background2) >= 4.0; 
           fragColor.a = (fragColor.a == 0.0 && backgroundAround) ? 0.996 : fragColor.a;
           return;
-        }
-        // non background border, handle holes.
-        vec4 nonzero = sign(z1 * z0);
-        float count = dot(nonzero, nonzero);
-        if (count > 0.0) {
-          float depth = dot(nonzero, mix(z1, z0, 0.5)) / count;
-          fragColor = vec4(c.rgb, depth);
+        }*/
+
+        // Same direction non-zero foreground including both pixels.
+        vec4 nzf1 = foreground1 * z0;
+        vec4 nzf2 = foreground2 * z1;
+        vec4 nzf11 = foreground11 * z01;
+        vec4 nzf21 = foreground21 * z11;
+        vec4 sdf1 = nzf1 * nzf11;
+        vec4 sdf2 = nzf2 * nzf21;
+
+        // Background or zero with different color.
+        const vec4 ZERO = vec4(0.0);
+        vec4 zero0 = vec4(equal(z0, ZERO));
+        vec4 zero1 = vec4(equal(z1, ZERO));
+        vec4 zero01 = vec4(equal(z01, ZERO));
+        vec4 zero11 = vec4(equal(z11, ZERO));
+        vec4 sdbz1 = (background1 + zero0) * (background11 + zero01);
+        vec4 sdbz2 = (background2 + zero1) * (background21 + zero11);
+
+        vec4 dc1 = vec4(1.0) - sim1;
+        vec4 dc2 = vec4(1.0) - sim2;
+        vec4 dc11 = vec4(1.0) - sim11;
+        vec4 dc21 = vec4(1.0) - sim21;
+
+        // Background in one direction with similar color as in background and
+        // then foreground of different color in opposite direction mark the
+        // pixel as background.
+        vec4 sdocfodbz1 = /*sdf1 * dc1 */ dc11 * sdbz2 * (sim2 * background2 + sim21 * background21);
+        vec4 sdocfodbz2 = /*sdf2 * dc2 */ dc21 * sdbz1 * (sim1 * background1 + sim11 * background11);
+        if (dot(sdocfodbz1, sdocfodbz1) + dot(sdocfodbz2, sdocfodbz2) > 0.0) {
+          fragColor = vec4(c.rgb, 1.0);
           return;
         }
+
+        // Special case: background or background followed by zero from left to 
+        // right with two zeros or background to the right is background.
+        if (c.a == 0.0 && sdbz1.a == 1.0 && (background2.a == 1.0 || (background21.a == 1.0 && zero1.a == 1.0))) {
+          fragColor.a = 0.9804;
+          return;          
+        }
+        
+        // Background around, patching smaller holes in background. TODO: same goes for patching foreground holes.
+        if (c.a == 0.0 && dot(background1, background1) + dot(background2, background2) >= 5.0) {
+          fragColor.a = 0.996;
+          return; 
+        }
+
+        // When we have a same direction foregrounds with the same color and in
+        // the same time, opposite direction background or zero of different color,
+        // let's make the pixel foreground.
+        vec4 sdscodbz1 = sdf1 * sim1 /* sim11*/ * sdbz2 * (vec4(1.0) - sim2) * (vec4(1.0) - sim21);
+        vec4 sdscodbz2 = sdf2 * sim2 /* sim21*/ * sdbz1 * (vec4(1.0) - sim1) * (vec4(1.0) - sim11);
+
+        if (c.a == 0.0 && dot(sdscodbz1.ga, sdscodbz1.ga) + dot(sdscodbz2.ga, sdscodbz2.ga) > 0.0)
+          fragColor = vec4(c.rgb, 0.4);
+
+        // non background border, handle holes.
+        vec4 nonzero = sign(nzf1 * nzf2);
+        vec4 nonzero1 = sign(nzf1 * nzf21);
+        vec4 nonzero2 = sign(nzf2 * nzf11);
+        vec4 nonzero3 = sign(nzf11 * nzf21);
+
+        bool nearBackground = any(greaterThan(background1 + background2 + background11 + background21, ZERO));
+        if (fragColor.a == 0.0 && /*!nearBackground &&*/ 
+        	(dot(nonzero.ga, nonzero.ga) == 2.0 || dot(nonzero1.ga, nonzero1.ga) == 2.0 || dot(nonzero2.ga, nonzero2.ga) == 2.0 || dot(nonzero3.ga, nonzero3.ga) == 2.0 ||
+        	 dot(nonzero.rb, nonzero.rb) == 2.0 || dot(nonzero1.rb, nonzero1.rb) == 2.0 || dot(nonzero2.rb, nonzero2.rb) == 2.0 || dot(nonzero3.rb, nonzero3.rb) == 2.0)) {
+          fragColor = vec4(c.rgb, 0.5);
+          return;
+        }
+        if (nearBackground && all(notEqual((background11 + zero01) * (background21 + zero11), ZERO))) {
+          fragColor.a = 0.973;
+          return;          
+        }
+
       }`;
     const renderVertex = `
       attribute vec2 v;
@@ -424,10 +544,14 @@ class DepthToColorSyncRender {
         vec4 tex = texture2D(s, t);
         gl_FragColor = (tex.a > range.x && tex.a < range.y) ? tex : vec4(0.0);
         if (tex.a == 1.0) {
-          gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+          gl_FragColor = vec4(1.0, 0.6, 0.1, 1.0);
         } else if (tex.a > 0.99) 
           gl_FragColor = vec4(1.0, 0.5, 0.5, 1.0);
-        else if (tex.a > 0.971) 
+        else if (tex.a > 0.9803) 
+          gl_FragColor = vec4(0.3, 0.8, 1.0, 1.0);
+        else if (tex.a > 0.972) 
+          gl_FragColor = vec4(0.5, 0.5, 0.7, 1.0);
+        else if (tex.a > 0.951) 
           gl_FragColor = vec4(0.0, 1.0, 0.0, 1.0);
         else if (tex.a == 0.0) 
            gl_FragColor = vec4(1.0, 1.0, 0.0, 1.0);
@@ -435,11 +559,16 @@ class DepthToColorSyncRender {
           gl_FragColor = vec4(0.0, 1.0, 1.0, 1.0);
         else if (tex.a < range.x) 
            gl_FragColor = vec4(1.0, 0.0, 1.0, 1.0);
+        else if (tex.a <= 0.4981 && tex.a > 0.49801) 
+          gl_FragColor = vec4(0.1, 0.5, 0.0, 1.0);
+        else if (tex.a >= 0.399 && tex.a <= 0.401) 
+          gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
 /*        if (tex.a > range.y)
           gl_FragColor = vec4(1.0);
         if (tex.a < range.x)
           gl_FragColor = vec4(1.0);*/
-        // gl_FragColor = tex;
+        gl_FragColor = tex;
+        gl_FragColor.a = 1.0;
       }`; 
 
     function createProgram(gl, vs, ps) {
